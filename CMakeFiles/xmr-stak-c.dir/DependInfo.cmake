594082817c4a9daff451af2f928aeec84e6fb462
root <root@digitalocean.(none)>
2017-09-11 13:40:10 +0700

    init

---
+++ CMakeFiles/xmr-stak-c.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/c_blake256.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/c_blake256.c.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/c_groestl.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/c_groestl.c.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/c_jh.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/c_jh.c.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/c_keccak.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/c_keccak.c.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/c_skein.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/c_skein.c.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/soft_aes.c" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/crypto/soft_aes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/xmr-stak-cpu.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/cli-miner.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/cli-miner.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/console.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/console.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/crypto/cryptonight_common.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/crypto/cryptonight_common.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/executor.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/executor.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/httpd.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/httpd.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/jconf.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/jconf.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/jpsock.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/jpsock.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/minethd.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/minethd.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/socket.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/socket.cpp.o"
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/webdesign.cpp" "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/webdesign.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/xmr-stak-cpu-master/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
